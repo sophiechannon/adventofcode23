13664c54b5291ed74ce641f85a0fccf24634b609
Sophie Gilder <sophieachannon@hotmail.co.uk>
2023-12-27 19:26:27 +0000

    day15 part 2

--- day15/HASH.test.ts
+++ day15/HASH.test.ts
@@ -3,10 +3,37 @@ import { HASH } from "./HASH";
 describe(HASH, () => {
   it("test", async () => {
     const hash = new HASH();
-    expect(await hash.getValueFromInput("day15/test")).toEqual(1320);
+    expect(await hash.getValueFromInput("day15/test")).toEqual(145);
+    expect(hash.hashmap).toEqual({
+      "0": [
+        {
+          focus: 1,
+          label: "rn",
+        },
+        {
+          focus: 2,
+          label: "cm",
+        },
+      ],
+      "1": [],
+      "3": [
+        {
+          focus: 7,
+          label: "ot",
+        },
+        {
+          focus: 5,
+          label: "ab",
+        },
+        {
+          focus: 6,
+          label: "pc",
+        },
+      ],
+    });
   });
   it("input", async () => {
     const hash = new HASH();
-    expect(await hash.getValueFromInput("day15/input")).toEqual(514281);
+    expect(await hash.getValueFromInput("day15/input")).toEqual(244199);
   });
 });
--- day15/HASH.ts
+++ day15/HASH.ts
@@ -1,13 +1,54 @@
 import { readFile } from "../utils/utils";
 
+type FocusObject = { label: string; focus: number };
+type HashMap = {
+  [key: number]: FocusObject[];
+};
+
 export class HASH {
+  hashmap: HashMap;
+  constructor() {
+    this.hashmap = {};
+  }
+
   async getValueFromInput(filename: string) {
     const strings = await this.parseFile(filename);
-    let total = 0;
     for (const string of strings) {
-      total += this.getTotalForString(string);
+      this.alignLens(string);
+    }
+    return this.getTotalFocusingPower();
+  }
+
+  private getTotalFocusingPower() {
+    let total = 0;
+    for (const box in this.hashmap) {
+      this.hashmap[box].forEach(
+        (value, index) => (total += (+box + 1) * (index + 1) * value.focus)
+      );
     }
     return total;
+  }
+  private alignLens(string: string) {
+    const focusObject = this.getFocusObject(string);
+    const box = this.getBox(focusObject.label);
+
+    if (!this.hashmap[box]) {
+      this.hashmap[box] = [];
+    }
+    const thisLabel = this.hashmap[box].findIndex(
+      (v) => v.label === focusObject.label
+    );
+    if (focusObject.focus) {
+      if (thisLabel !== -1) {
+        this.hashmap[box][thisLabel].focus = focusObject.focus;
+      } else {
+        this.hashmap[box].push(focusObject);
+      }
+    } else {
+      if (thisLabel !== -1) {
+        this.hashmap[box].splice(thisLabel, 1);
+      }
+    }
   }
 
   private async parseFile(filename: string) {
@@ -19,7 +60,7 @@ export class HASH {
     return strings;
   }
 
-  private getTotalForString(string: string) {
+  private getBox(string: string) {
     let currentValue = 0;
     const asciiValues = this.getAsciiValues(string);
     asciiValues.forEach((value) => {
@@ -32,5 +73,11 @@ export class HASH {
 
   private getAsciiValues(string: string) {
     return string.split("").map((char) => char.charCodeAt(0));
+  }
+
+  private getFocusObject(string: string): FocusObject {
+    const label = string.split(/[=-]/)[0];
+    const focus = +string.split(/[=-]/)[1];
+    return { label, focus };
   }
 }
